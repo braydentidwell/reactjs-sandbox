8fcecbbbf6789e18dddd8e391d57fdb51ffb7a00
btidwell <btidwell@axway.com>
2015-10-21 11:51:17 -0700

    Added Gulp build

--- comments-tut/gulpfile.js
+++ comments-tut/gulpfile.js
@@ -5,7 +5,7 @@ var source = require('vinyl-source-stream');
  
 gulp.task('build', function () {
   browserify({
-    entries: 'index.html',
+    entries: 'js/components/comments/comments.react.js',
     extensions: ['.js'],
     debug: true
   })
